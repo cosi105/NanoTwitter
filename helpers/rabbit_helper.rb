05cdff860250fdbed4f8d33d0fa54b74b1fac2c0
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-17 11:42:06 -0400

    Outlines ToDos in tweet_routes.rb

--- helpers/rabbit_helper.rb
+++ helpers/rabbit_helper.rb
@@ -1,1 +1,12 @@
 # Holds RabbitMQ-related helper functions
+
+# Initialize RabbitMQ object & connection appropriate to
+# the current env (viz., local vs. prod)
+def get_rabbit_object
+  if Sinatra::Base.production?
+    # Determine what env variable(s) are needed in prod
+  else
+    rabbit = Bunny.new(automatically_recover: false)
+  end
+  rabbit.start
+end
