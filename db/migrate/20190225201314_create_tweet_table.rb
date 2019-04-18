ea80ce8268129d10c8d605c045a4dc92aa92c747
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-18 00:46:32 -0400

    Needed that migrate...

---
+++ db/migrate/20190225201314_create_tweet_table.rb
@@ -0,0 +1,11 @@
+# Initializes the db's Tweet table
+class CreateTweetTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.string :body
+      t.datetime :created_on
+      t.integer :author_id
+      t.string :author_handle
+    end
+  end
+end
