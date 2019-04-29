b693feb13d806ae4605291e68556aa9404303699
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-29 02:16:48 -0400

    ` with '#' will be ignored, and an empty message aborts the commit. `go away ` On branch brad ` Changes to be committed: `	modified:   helpers/redis_helper.rb `	modified:   helpers/seed_helper.rb `

--- helpers/seed_helper.rb
+++ helpers/seed_helper.rb
@@ -3,28 +3,26 @@ require_relative 'rabbit_helper'
 require_relative 'redis_helper'
 
 # Follows seeding
-def publish_follows_seeds
-    puts 'Starting seeding!'
-    follow_data_seed = CHANNEL.queue('follow.data.seed')
-    follows_data_payload = []
-    Follow.all.each do |f|
-      follows_data_payload << {
-        follower_id: f.follower_id,
-        follower_handle: f.follower_handle,
-        followee_id: f.followee_id,
-        followee_handle: f.followee_handle
-      }
-    end
-    publish(follow_data_seed, follows_data_payload)
-    puts 'Finished seeding Follows!'
+def publish_follow_data_seed
+  puts 'Starting seeding!'
+  follow_data_seed = CHANNEL.queue('follow.data.seed')
+  follows_data_payload = []
+  Follow.all.each do |f|
+    follows_data_payload << {
+      follower_id: f.follower_id,
+      follower_handle: f.follower_handle,
+      followee_id: f.followee_id,
+      followee_handle: f.followee_handle
+    }
+  end
+  publish(follow_data_seed, follows_data_payload.to_json)
+  puts 'Finished seeding Follows!'
 end
 
 # Tweets seeding: TweetHTML & Searcher
-def publish_tweet_seeds
-  tweet_html_payload = []
-  searcher_payload = []
-  tweet_html_router_seed = CHANNEL.queue('tweet.html.router.seed')
-  searcher_seed = CHANNEL.queue('searcher.seed')
+def publish_tweet_data_seed
+  tweet_data_seed = CHANNEL.queue('tweet.data.seed')
+  tweet_data_payload = []
   Tweet.all.each do |t|
     tweet_html_payload << {
       author_id: t.author_id,
@@ -33,19 +31,14 @@ def publish_tweet_seeds
       tweet_body: t.body,
       tweet_created: t.created_on
     }
-    searcher_payload << {
-      tweet_id: t.id,
-      tweet_body: t.body
-    }
   end
-  publish(tweet_html_router_seed, tweet_html_payload)
-  publish(searcher_seed, searcher_payload)
+  publish(tweet_data_seed, tweet_data_payload.to_json)
   puts 'Finished seeding Tweets!'
 end
 
 # Timeline seeding: TimelineData
 def publish_timeline_seeds
-  timeline_data_seed = CHANNEL.queue('tweet.data.seed')
+  timeline_data_seed = CHANNEL.queue('timeline.data.seed')
   payload = []
   User.all.each do |user|
     tweet_data = []
@@ -64,6 +57,17 @@ def publish_timeline_seeds
   puts 'Finished seeding Tweet data!'
 end
 
-publish_follows_seeds
-publish_tweet_seeds
-publish_timeline_seeds
+# seed_follows
+# seed_tweets
+# seed_timelines
+
+[REDIS_FOLLOW_HTML, REDIS_SEARCH_HTML, REDIS_TIMELINE_HTML, REDIS_USER_DATA].each(&:flushall)
+
+# Pre-caches a mapping of all user handles to user ids
+require_relative '../models/user'
+Thread.new do
+  User.all.each do |user|
+    REDIS.set("#{user.handle}:user_id", user.id)
+    user.tweets.each { |tweet| REDIS.lpush("#{user.handle}:tweet_ids", tweet.id) }
+  end
+end
