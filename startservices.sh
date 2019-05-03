2b76bd400efa1980adfe77a0dd445cb5362499c6
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-05-03 12:11:32 -0400

    Adds port 6391 for new search_data Redis shard

--- startservices.sh
+++ startservices.sh
@@ -24,6 +24,7 @@
 echo "Spinning up Redis servers in background processes..."
 redis-server &
 redis-server --port 6390 &
+redis-server --port 6391 &
 ports='0 1 2 3 4 5 6 7 8 9'
 for port in $ports
 do
